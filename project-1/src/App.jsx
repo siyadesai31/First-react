4006d9a932b840dab345bd6bbd151d088da991b0
Siya <siyadesai31@gmail.com>
2024-06-01 00:15:55 +0530

    Navigation

---
+++ project-1/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import Navigation from "./components/navigation";"./components/navigation.jsx"
+
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+    </div>
+  );
+};
+
+export default App;
