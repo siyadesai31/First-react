1047ff24c426b1c1d8f7ff9b8e02dde9ac707756
Siya <siyadesai31@gmail.com>
2024-06-01 17:05:19 +0530

    Hero Section

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
-import Navigation from "./components/navigation";"./components/navigation.jsx"
+import Navigation from "./components/navigation";
+import HeroSection from "./components/Hero";
 
 const App = () => {
   return (
     <div>
       <Navigation/>
+      <HeroSection />
     </div>
   );
 };
